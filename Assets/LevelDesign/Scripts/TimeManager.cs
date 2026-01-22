f7dd1d0844af40191fced997738a0db98f91b675
ShuTanaka <251320@st.yoshida-g.ac.jp>
2026-01-22 15:14:53 +0900

    TimeManagerでのタイマーを作りました

--- Assets/LevelDesign/Scripts/TimeManager.cs
+++ Assets/LevelDesign/Scripts/TimeManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
-    public Text timeText;    //時間表示用テキスト
+    public TextMeshProUGUI timeText;    //時間表示用テキスト
     public float limit = 30.0f;    //制限時間
     public GameObject text;    //ゲームオーバー表示用テキスト
     public GameObject player;    //プレイヤー格納用
